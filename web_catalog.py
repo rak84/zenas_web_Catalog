a53d46e661154afbb8166cc8bfa25fded2755b89
rak84 <121898830+rak84@users.noreply.github.com>
2023-04-25 14:44:47 -0500

    Update web_catalog.py

--- web_catalog.py
+++ web_catalog.py
@@ -25,7 +25,7 @@ color_list = df[0].values.tolist()
 #print(color_list)
 
 #Let's put a pick list here to they can pick the color
-option = streamlit.selectbox('Pick a sweatsuit color or style:') , list(color_list))
+option = streamlit.selectbox('Pick a sweatsuit color or style:' , list(color_list))
 
 #We'll build the image caption now, since we can
 product_caption = 'Our warm, comfirtable, '+ option + 'sweatsuit!'
